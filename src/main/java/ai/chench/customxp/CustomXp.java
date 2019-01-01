fa9957eeb8850b015a9b4d5e44ea6675d8803d88
ChenChai <chen.chai@outlook.com>
2019-01-01 11:59:52 -0700

    Initial Commit

---
+++ src/main/java/ai/chench/customxp/CustomXp.java
@@ -0,0 +1,14 @@
+package ai.chench.customxp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomXp extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+    @Override
+    public void onDisable() {
+
+    }
+}
